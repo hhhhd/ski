43697f679af131f6615f87905fb4bd13f14f1f4f
Korui <a1665083753@gmail.com>
2017-09-02 23:19:58 +0800

    feat/switching

--- src/components/switching/index.js
+++ src/components/switching/index.js
@@ -5,6 +5,7 @@ const template = require('./template.html')
 require('./style.less')
 
 @Component({
+  template,
   model: {
     prop: 'checked',
     event: 'change'
@@ -12,22 +13,10 @@ require('./style.less')
   props: {
     checked: Boolean,
   },
-  template,
 })
 export default class Switching extends Vue {
-  isOn = false
-  isOff = true
 
   emitClick() {
-    // this.$emit('on-off', this.switching)
-    if (this.isOn === false) {
-      this.isOn = true
-      this.isOff = false
-      this.$emit('change', this.isOn);
-    } else {
-      this.isOn = false
-      this.isOff = true
-      this.$emit('change', this.isOn);
-    }
+    this.$emit('change', !this.checked)
   }
 }
