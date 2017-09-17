94275b0a7808d73ad65315acbf3871c99a4160eb
Korui <a1665083753@gmail.com>
2017-09-17 14:33:18 +0800

    feat: updateCheckbox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import button from './components/button'
 import input from './components/input'
 import switching from './components/switching'
-import checkbox from './components/checkbox'
 import checkboxGroup from './components/checkboxGroup'
 
 export {
   checkboxGroup,
-  checkbox,
   button,
   input,
   switching
