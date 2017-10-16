e514999bb0ed96f9ac3b50582d1c231f788d19c5
Korui <a1665083753@gmail.com>
2017-10-16 23:51:05 +0800

    feature/alert

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import button from './components/button'
 import input from './components/input'
 import switching from './components/switching'
 import checkboxGroup from './components/checkboxGroup'
+import alert from './components/alert'
 
 export {
   checkboxGroup,
   button,
   input,
-  switching
+  switching,
+  alert
 }
