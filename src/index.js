c088ab97fa6628763c4beebe7bc5de06981ac43c
Korui <a1665083753@gmail.com>
2017-10-18 23:26:04 +0800

    feat/toast

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import button from './components/button'
 import input from './components/input'
 import switching from './components/switching'
 import checkboxGroup from './components/checkboxGroup'
+import toast from './components/toast'
 
 export {
+  toast,
   checkboxGroup,
   button,
   input,
