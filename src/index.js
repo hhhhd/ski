0de61b47399e51aa684544f8d2d07124bc6c4a83
Korui <a1665083753@gmail.com>
2017-10-20 14:01:17 +0800

    feat:header

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import button from './components/button'
 import input from './components/input'
 import switching from './components/switching'
 import checkboxGroup from './components/checkboxGroup'
+import header from './components/header'
+
 
 export {
   checkboxGroup,
   button,
   input,
-  switching
+  switching,
+  header
 }
