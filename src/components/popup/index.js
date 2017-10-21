f3932a0c979655ce87f0aa4526616abdfc507e4b
Korui <a1665083753@gmail.com>
2017-10-21 22:34:02 +0800

    feat:popup

---
+++ src/components/popup/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+const template = require('./template.html')
+require('./style.less')
+
+@Component({
+  template,
+  props: {
+    msg: String,
+    isShow: Boolean,
+  }
+})
+export default class Button extends Vue {
+  close() {
+    this.isShow = false
+  }
+}
