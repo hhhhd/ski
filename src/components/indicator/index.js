d07b97a378d8238299baf3a3eddfeea2081f10a5
Korui <a1665083753@gmail.com>
2017-10-27 21:49:04 +0800

    feat:indicator

---
+++ src/components/indicator/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+const template = require('./template.html')
+require('./style.less')
+
+@Component({
+  template,
+  props: {
+    showText: Boolean,
+  }
+})
+export default class indicator extends Vue {}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import button from './components/button'
 import input from './components/input'
 import switching from './components/switching'
+import indicator from './components/indicator'
 import checkboxGroup from './components/checkboxGroup'
 
 export {
   checkboxGroup,
   button,
   input,
-  switching
+  switching,
+  indicator,
 }
