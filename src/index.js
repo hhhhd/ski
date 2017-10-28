a8b19c3363926827b337c298efe25b01c2396494
Korui <a1665083753@gmail.com>
2017-10-28 21:53:34 +0800

    feat:range

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import button from './components/button'
 import input from './components/input'
 import switching from './components/switching'
+import range from './components/range'
 import checkboxGroup from './components/checkboxGroup'
 
 export {
   checkboxGroup,
   button,
   input,
-  switching
+  switching,
+  range
 }
