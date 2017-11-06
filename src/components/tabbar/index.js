91e9e89a0ba4ced1b53990897a7d3655e9b96e67
Korui <a1665083753@gmail.com>
2017-11-06 22:40:11 +0800

    feat:tabbar

---
+++ src/components/tabbar/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+const template = require('./template.html')
+require('./style.less')
+
+@Component({
+  template
+})
+export default class Button extends Vue {
+  SOne = true
+  STwo = false
+  SThree = false
+  SFour = false
+  showOne() {
+    this.SOne = true
+    this.STwo = false
+    this.SThree = false
+    this.SFour = false
+  }
+  showTwo() {
+    this.SOne = false
+    this.STwo = true
+    this.SThree = false
+    this.SFour = false
+  }
+  showThree() {
+    this.SOne = false
+    this.STwo = false
+    this.SThree = true
+    this.SFour = false
+  }
+  showFour() {
+    this.SOne = false
+    this.STwo = false
+    this.SThree = false
+    this.SFour = true
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import button from './components/button'
 import input from './components/input'
 import switching from './components/switching'
+import tabbar from './components/tabbar'
 import checkboxGroup from './components/checkboxGroup'
 
 export {
   checkboxGroup,
   button,
   input,
-  switching
+  switching,
+  tabbar
 }
