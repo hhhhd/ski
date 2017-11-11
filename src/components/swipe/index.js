e60d70e516c4cfa6b75376b9fb8efd7b745dd429
Korui <a1665083753@gmail.com>
2017-11-11 22:18:16 +0800

    feat:swipe

--- src/components/swipe/index.js
+++ src/components/swipe/index.js
@@ -12,8 +12,13 @@ export default class Swipe extends Vue {
   isTrueOne = true
   isTrueTwo = false
   isTrueThree = false
+  isRoundOne = true
+  isRoundTwo = false
+  isRoundThree = false
+  intervalId = null
   mounted() {
-    setInterval(this.GoScroll, 2000)
+    this.GoScroll()
+    this.intervalId = setInterval(this.GoScroll, 9000)
   }
   GoScroll() {
     setTimeout(() => {
@@ -21,19 +26,51 @@ export default class Swipe extends Vue {
       this.isTrueOne = true
       this.isTrueTwo = false
       this.isTrueThree = false
-      console.log('第二步')
+      this.isRoundOne = true
+      this.isRoundTwo = false
+      this.isRoundThree = false
       setTimeout(() => {
         console.log('第三步')
         this.isTrueOne = false
         this.isTrueTwo = true
         this.isTrueThree = false
+        this.isRoundOne = false
+        this.isRoundTwo = true
+        this.isRoundThree = false
         setTimeout(() => {
-          console.log('第四步')
+          console.log('第五步')
           this.isTrueOne = false
           this.isTrueTwo = false
           this.isTrueThree = true
-        }, 4000);
-      }, 2000);
+          this.isRoundOne = false
+          this.isRoundTwo = false
+          this.isRoundThree = true
+        }, 3000);
+      }, 3000);
     }, 0);
+  }
+  toOne() {
+    this.isTrueOne = true
+    this.isTrueTwo = false
+    this.isTrueThree = false
+    this.isRoundOne = true
+    this.isRoundTwo = false
+    this.isRoundThree = false
+  }
+  toTwo() {
+    this.isTrueOne = false
+    this.isTrueTwo = true
+    this.isTrueThree = false
+    this.isRoundOne = false
+    this.isRoundTwo = true
+    this.isRoundThree = false
+  }
+  toThree() {
+    this.isTrueOne = false
+    this.isTrueTwo = false
+    this.isTrueThree = true
+    this.isRoundOne = false
+    this.isRoundTwo = false
+    this.isRoundThree = true
   }
 }
