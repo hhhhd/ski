eacb41cb0ee53585fc54570043aa93ebf624c56d
Hugo <hugojing@users.noreply.github.com>
2017-11-16 00:46:03 -0600

    feat: toast

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import input from './components/input'
 import switching from './components/switching'
 import tabbar from './components/tabbar'
 import checkboxGroup from './components/checkboxGroup'
+import toast from './components/toast'
 import alert from './components/alert'
 import loading from './components/loading'
 
 export {
+  toast,
   checkboxGroup,
   button,
   input,
