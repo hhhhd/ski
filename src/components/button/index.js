dbbfd63e5fef1608afd69b02eb2179782af1c6d1
Korui <a1665083753@gmail.com>
2017-12-16 23:00:41 +0800

    feat:button(update)

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -11,31 +11,4 @@ require('./style.less')
     size: String
   }
 })
-export default class Button extends Vue {
-  showDefault = null
-  showdanger = null
-  showPrimary = null
-  sizeSml = null
-  sizeMid = null
-  sizeBig = null
-  mounted() {
-    if (this.type === 'Default' || this.type === 'default') {
-      this.showDefault = true
-    } else if (this.type === 'Danger' || this.type === 'danger') {
-      this.showdanger = true
-    } else if (this.type === 'Primary' || this.type === 'primary') {
-      this.showPrimary = true
-    } else {
-      console.log('请输入Big、big、Sml、sml、Mid、mid')
-    }
-    if (this.size === 'Sml' || this.size === 'sml') {
-      this.sizeSml = true
-    } else if (this.size === 'Mid' || this.size === 'mid') {
-      this.sizeMid = true
-    } else if (this.size === 'Big' || this.size === 'big') {
-      this.sizeBig = true
-    } else {
-      console.log('请输入Big、big、Sml、sml、Mid、mid')
-    }
-  }
-}
+export default class Button extends Vue {}
