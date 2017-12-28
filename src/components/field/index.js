d2772526e0cd4a679f08f2981b91ffb859717868
Korui <a1665083753@gmail.com>
2017-12-28 22:57:23 +0800

    feat:field

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -14,44 +14,6 @@ require('./style.less')
 })
 export default class Field extends Vue {
   value = null
-  Stext = false
-  Sbirthday = false
-  Stextarea = false
-  Snumber = false
-  Sverification = false
-  mounted() {
-    if (this.type === 'text') {
-      this.Stext = true
-      this.Sbirthday = false
-      this.Stextarea = false
-      this.Snumber = false
-      this.Sverification = false
-    } else if (this.type === 'birthday') {
-      this.Stext = false
-      this.Sbirthday = true
-      this.Stextarea = false
-      this.Snumber = false
-      this.Sverification = false
-    } else if (this.type === 'self') {
-      this.Stext = false
-      this.Sbirthday = false
-      this.Stextarea = true
-      this.Snumber = false
-      this.Sverification = false
-    } else if (this.type === 'number') {
-      this.Stext = false
-      this.Sbirthday = false
-      this.Stextarea = false
-      this.Snumber = true
-      this.Sverification = false
-    } else if (this.type === 'verification') {
-      this.Stext = false
-      this.Sbirthday = false
-      this.Stextarea = false
-      this.Snumber = false
-      this.Sverification = true
-    }
-  }
   Svalue(e) {
     this.value = e.target.value
     this.$emit('getValue', this.value)
